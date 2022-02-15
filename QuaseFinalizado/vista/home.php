3cd810a4982a6aa30cc0f204952b733df37eb81d
Leonardo M <95316458+FireWolf014@users.noreply.github.com>
2022-02-15 20:46:14 -0300

    Create home.php

---
+++ QuaseFinalizado/vista/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+</head>
+<body>
+    <h1>Teste Titulo</h1>
+    <br><br>
+    <a href="index.php?classe=ConteinerController&metodo=abrirCadastro">Contêiner</a><br>
+    <a href="index.php?classe=MovimentacaoController&metodo=abrirCadastro">Movimentações</a><br>
+    <a href="index.php?classe=ConsultaController&metodo=abrirConsulta">Relatório</a><br>
+    <a href="index.php?classe=ConteinerController&metodo=abrirConsulta">Editar Contêiner</a><br>
+    <a href="index.php?classe=MovimentacaoController&metodo=abrirConsulta">Editar Movimentações</a>
+
+</body>
+</html>
